9a68d2d3936ab289cbdef0a28b8f1f57bd380cac
root <root@us-wpl-c7-km231.wpl.host>
2018-04-30 06:35:30 -0500

    different cursors now perform different actions. also now passing noun names by a more copy/paste friendly "this.innerHTML" added some more text to ROOM to show off looking, grabing, opening, talking, and moving. script now oficially in the script file, while game veriables and functions are in the main js.

--- PaTA.js
+++ PaTA.js
@@ -1,13 +1,27 @@
+var messagelog = document.getElementById('messagelog');
+var currentCursor = "look";
+var playerLocation = "bed";
+
 function initiate() {
 	display(opening);
 }
 function c(noun) {
-	switch(noun) {
-		case "nBed":
-			display(LookBed);
+	switch(currentCursor) {
+		case "look":
+			if (noun == "room") display(lookRoom);
+			if (noun == "bed") display(lookBed);
 			break;
-		case "nRoom":
-			display(LookRoom);
+		case "grab":
+			if (noun == "room") display(grabRoom);
+			break;
+		case "open":
+			if (noun == "room") display(openRoom);
+			break;
+		case "talk":
+			if (noun == "room") display(talkRoom);
+			break;
+		case "move":
+			if (noun == "room") display(moveRoom);
 	}
 }
 function display(textBlock) {
@@ -15,14 +29,30 @@ function display(textBlock) {
 	messagelog.scrollTop = messagelog.scrollHeight;
 }
 function btnClick(btnId) {
-	document.getElementById('btnLook').style.display = (btnId == "btnLook") ? "none" : "initial";
-	document.getElementById('btnGrab').style.display = (btnId == "btnGrab") ? "none" : "initial";
-	document.getElementById('btnOpen').style.display = (btnId == "btnOpen") ? "none" : "initial";
-	document.getElementById('btnTalk').style.display = (btnId == "btnTalk") ? "none" : "initial";
-	document.getElementById('btnMove').style.display = (btnId == "btnMove") ? "none" : "initial";
-	if (btnId == "btnLook") document.body.style.cursor = "url('images/pataLookCur.png'), auto";
-	if (btnId == "btnGrab") document.body.style.cursor = "url('images/pataGrabCur.png'), auto";
-	if (btnId == "btnOpen") document.body.style.cursor = "url('images/pataOpenCur.png'), auto";	
-	if (btnId == "btnTalk") document.body.style.cursor = "url('images/pataTalkCur.png'), auto";
-	if (btnId == "btnMove") document.body.style.cursor = "url('images/pataMoveCur.png'), auto";
+	console.log(btnId);
+	document.getElementById('btnLook').style.display = (btnId == "btnLook" || btnId == "btnLookDown") ? "none" : "initial";
+	document.getElementById('btnGrab').style.display = (btnId == "btnGrab" || btnId == "btnGrabDown") ? "none" : "initial";
+	document.getElementById('btnOpen').style.display = (btnId == "btnOpen" || btnId == "btnOpenDown") ? "none" : "initial";
+	document.getElementById('btnTalk').style.display = (btnId == "btnTalk" || btnId == "btnTalkDown") ? "none" : "initial";
+	document.getElementById('btnMove').style.display = (btnId == "btnMove" || btnId == "btnMoveDown") ? "none" : "initial";
+	if (btnId == "btnLook") { 
+		document.body.style.cursor = "url('images/pataLookCur.png'), auto";
+		currentCursor = "look";
+	}
+	if (btnId == "btnGrab") { 
+		document.body.style.cursor = "url('images/pataGrabCur.png'), auto";
+		currentCursor = "grab";
+	}
+	if (btnId == "btnOpen") {
+		document.body.style.cursor = "url('images/pataOpenCur.png'), auto";	
+		currentCursor = "open";
+	}
+	if (btnId == "btnTalk") {
+		document.body.style.cursor = "url('images/pataTalkCur.png'), auto";
+		currentCursor = "talk";
+	}
+	if (btnId == "btnMove") {
+		document.body.style.cursor = "url('images/pataMoveCur.png'), auto";
+		currentCursor = "move";
+	}
 }
---
+++ script.js
@@ -0,0 +1,9 @@
+var opening = '<p>You wake in an unfamilure <a onclick="c(this.innerHTML)">bed</a> in a dimly lit <a onclick="c(this.innerHTML)">room</a>.</p>';
+//room
+var lookRoom = '<p>You look around the room. It is dimly lit by moonlight though the window. There is a door oposite window, and a dresser on the wall oposite the <a onclick="c(this.innerHTML)">bed</a>.</p>';
+var grabRoom = '<p>The room feels unfamilure, dark and foreboding, The night air is cool.</p>';
+var openRoom = '<p>The room is spacious and uncluttered, but you will need to open the [door], to open the room.</p>';
+var talkRoom = '<p>You call out to the empty room. There is no response.</p>';
+var moveRoom = '<p>You pace the room, uncertain of your situation.</p>';
+//bed
+var lookBed = '<p>The bed is an ornate four poster. The sheets are of a fine linen, if a bit old.</p>';
